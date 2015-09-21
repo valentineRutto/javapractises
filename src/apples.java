36c8dd62a95d9b235f24edffe5285e7fc6d84afb
valentinerutto <rutto28@gmail.com>
2015-09-21 15:15:30 +0300

    called mylove method with parameter on mainclass

--- src/apples.java
+++ src/apples.java
@@ -1,9 +1,15 @@
-
+import java.util.Scanner;
 public class apples {
 	public static void main (String[] args){
 		//create object of the class
 		methods methodsObject =new methods();
 		methodsObject.message();
+		//scanner method for the method mylove
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the important character trait of your crush:");
+		String name=input.nextLine();//set input to be stored in var name from methods class
+		
+		methodsObject.mylove(name);
 	}
 
 }
--- src/methods.java
+++ src/methods.java
@@ -3,5 +3,8 @@ public class methods {
 	public void message(){
 		System.out.println("almost selfsabotaging");
 	}
-
+public void mylove(String name){
+	System.out.println(name+" Really that ppplleeasse go find him/her on ebay....!!!!!!....!");
+	
+}
 }
